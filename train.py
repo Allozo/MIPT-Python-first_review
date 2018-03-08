4aeea18ee7ffaf5a765cc495a5ea4a25f51726ce
allozo <allozo@yandex.ru>
2018-03-08 14:18:47 +0300

    добавил считывание слов, разбитие их на пары, но пока что нет учета частотности

--- train.py
+++ train.py
@@ -2,14 +2,7 @@ import pymorphy2
 import re
 
 text = "wp.txt"
-
-morph = pymorphy2.MorphAnalyzer()
-analysis = morph.parse("hello")[0]
-print(analysis)
-analysis = morph.parse("привет")[0]
-print(analysis)
-
-
+diction = dict()                                    # тут будут храниться связки слов
 with open(text, "r", encoding="UTF-8") as file:
     for line in file:
         if line != '\n':
@@ -18,6 +11,9 @@ with open(text, "r", encoding="UTF-8") as file:
             line = line.split()                     # получили простой лист слов
             size = len(line)
             for i in range(size):
-                if i != size:
+                if i != size - 1:
                     word = line[i]
                     next_word = line[i + 1]
+                    diction.setdefault(word, list())     # можно написать if word in diction: -//-?
+                    diction[word].append(next_word)
+print(diction)
