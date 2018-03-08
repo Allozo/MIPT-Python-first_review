734d8ca8ab6963d5eaa77fc4dc53514f83eeee12
allozo <allozo@yandex.ru>
2018-03-08 18:00:21 +0300

    есть все кроме реализации консольного интерфейса

---
+++ generate.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+text = "model.txt"
+diction = dict()
+with open(text, "r", encoding="UTF-8") as file:
+    for line in file:
+        line = line.split()
+        word = line[0]
+        next_word = line[1]
+        frequency = int(line[2])
+        if word in diction:
+                diction[word].setdefault(next_word, frequency)
+        else:
+            diction.setdefault(word, {next_word: frequency})
+# загрузили наш словарь
+
+
+print("Введите начальное слово")
+word = input()
+print("Введите длину последовательности")
+size = int(input())
+new_text = [word]
+for i in range(size - 1):
+    listik = []
+    frequency = []
+    number = 0
+    for two_word in diction[new_text[i]]:      # пробегаемся по всем вторым словам
+        number += diction[new_text[i]][two_word]
+        listik.append(two_word)
+    for j in range(len(listik)):
+        frequency.append(diction[new_text[i]][listik[j]] / number)
+    np.random.choice(listik, len(listik), frequency)
+    selected_word = listik[0]
+    new_text.append(selected_word)
+for words in new_text:
+    print(words, end=" ")
