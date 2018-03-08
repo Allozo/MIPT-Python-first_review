0a1e30ab4576dad3f2a6efe6a872991c9919474a
allozo <allozo@yandex.ru>
2018-03-08 16:13:40 +0300

    добавил частотность "word_1" - "word_2" - "частота" сохраняется в файл model.txt нет реализации "консольного интерфейса"

--- train.py
+++ train.py
@@ -1,19 +1,27 @@
-import pymorphy2
 import re
 
 text = "wp.txt"
-diction = dict()                                    # тут будут храниться связки слов
+diction = dict()                            # тут будут храниться связки слов
 with open(text, "r", encoding="UTF-8") as file:
     for line in file:
         if line != '\n':
-            line = line.lower()                     # приводим к нижнему регистру
-            line = re.sub(r'[^\w\s]', ' ', line)    # избавляемся от пробелов/ знаков препинания
-            line = line.split()                     # получили простой лист слов
+            line = line.lower()  # приводим к нижнему регистру
+            line = re.sub(r'[^\w\s]', ' ', line)  # избавляемся от пробелов/ знаков препинания
+            line = line.split()  # получили простой лист слов
             size = len(line)
             for i in range(size):
                 if i != size - 1:
                     word = line[i]
                     next_word = line[i + 1]
-                    diction.setdefault(word, list())     # можно написать if word in diction: -//-?
-                    diction[word].append(next_word)
-print(diction)
+                    if word in diction:
+                        if next_word in diction[word]:
+                            diction[word][next_word] = diction[word][next_word] + 1  # +1 к частоте
+                        else:
+                            diction[word].setdefault(next_word, 1)
+                    else:
+                        diction.setdefault(word, {next_word: 1})  # просто добавили слово
+
+with open("model.txt", "w", encoding="UTF-8") as f:
+    for i in diction:
+        for j in diction[i]:
+            f.write(i + " " + j + " " + str(diction[i][j]) + '\n')
